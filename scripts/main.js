f240010f04f04357af3deeed25c98dae7a14dddd
TamerlanG <tamerlanalbehairy@gmail.com>
2019-08-28 18:20:03 +0600

    Add grid blocks

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,19 @@
-function createGrid(size) {}
+function createGrid() {
+  let size = prompt("Please input size of grid");
+  let totalBlocks = Math.pow(size, 2);
+  let mainContainer = document.getElementById("main-wrapper");
+  let div;
 
-function styleDiv(divToStyle, sizeOfPixels) {
+  for (i = 0; i < totalBlocks; i++) {
+    div = document.createElement("div");
+    styleBlock(div, size);
+    mainContainer.appendChild(div);
+  }
+}
+
+function styleBlock(divToStyle, sizeOfPixels) {
+  let gridStylesheets = document.styleSheets[1];
   divToStyle.style.width = sizeOfPixels + "px";
   divToStyle.style.height = sizeOfPixels + "px";
-  divToStyle.classList.add("grid");
+  divToStyle.classList.add("block");
 }
