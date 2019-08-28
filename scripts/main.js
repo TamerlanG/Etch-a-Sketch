a55965de288a39191793418c0efa40dda5439179
TamerlanG <tamerlanalbehairy@gmail.com>
2019-08-28 19:45:26 +0600

    Add add table only for 16x16 tables

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ function createGrid() {
   let mainContainer = document.getElementById("main-wrapper");
   let div;
 
-  for (i = 0; i < totalBlocks; i++) {
+  for (i = 1; i <= totalBlocks; i++) {
     div = document.createElement("div");
     styleBlock(div, size);
     mainContainer.appendChild(div);
@@ -12,8 +12,5 @@ function createGrid() {
 }
 
 function styleBlock(divToStyle, sizeOfPixels) {
-  let gridStylesheets = document.styleSheets[1];
-  divToStyle.style.width = sizeOfPixels + "px";
-  divToStyle.style.height = sizeOfPixels + "px";
   divToStyle.classList.add("block");
 }
