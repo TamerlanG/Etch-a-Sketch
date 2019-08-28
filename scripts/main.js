86d7596d92c847838d8a6812b6b53a5af7d25bce
TamerlanG <tamerlanalbehairy@gmail.com>
2019-08-28 15:35:51 +0600

    Add styleDiv

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function createGrid(size) {}
+
+function styleDiv(divToStyle, sizeOfPixels) {
+  divToStyle.style.width = sizeOfPixels + "px";
+  divToStyle.style.height = sizeOfPixels + "px";
+  divToStyle.classList.add("grid");
+}
